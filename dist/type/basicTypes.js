a4d34596d47e5c8838f2709b67ba5e8d28b20091
eunjeong <poil_chxn@naver.com>
2021-02-25 00:06:07 +0900

    interface

---
+++ dist/type/basicTypes.js
@@ -0,0 +1,36 @@
+var stringValue;
+var numberValue;
+var booleanValue;
+var undefinedValue;
+var nullValue;
+var objectValue;
+var symbolValue;
+var anyValue;
+numberValue = 3.3;
+stringValue = "hello";
+stringValue = 'hello';
+stringValue = "hello";
+booleanValue = true;
+stringValue = undefined;
+stringValue = null;
+objectValue = undefined;
+objectValue = null;
+anyValue = 1;
+anyValue = '1';
+anyValue = {};
+objectValue = {};
+objectValue = { name: 'ej' };
+objectValue = new String('hello');
+symbolValue = Symbol();
+var nameList;
+nameList = ["1", "3"];
+var user;
+user = {
+    name: 'ej',
+    score: 30
+};
+var tuple2;
+var tuple3;
+tuple2 = [1, 'ㅇ'];
+tuple3 = [1, 'ㅇ', 3];
+//# sourceMappingURL=basicTypes.js.map
---
+++ src/type/interface.ts
@@ -0,0 +1,52 @@
+interface TV {
+//행위 기술 = 메서드를 기술합니다.
+turnOn(): boolean; 
+turnOff(): void;//아무것도 반환받지 않는다는 의미입니당
+}
+
+const myTV : TV = {
+  turnOn() {
+    return true;
+  },
+  turnOff() {
+
+  }
+}
+
+function tryTurnOn(tv : TV) {
+  tv.turnOn
+}
+
+tryTurnOn(myTV);
+
+//속성만 정의된 인터페이스
+interface Cell {
+  row: number;
+  col : number;
+  piece? : Piece;
+}
+//행위가 정의된 인터페이스
+interface Piece {
+  move(from: Cell, to:Cell) : boolean;
+}
+
+
+function createBoard () {
+  const cells: Cell[] = [];
+  for(let row = 0; row < 4; row++) {
+    for(let col = 0; col < 3; col++) {
+      cells.push({
+        row, col
+      })
+    }
+  }
+
+  return cells;
+}
+
+const board = createBoard();
+board[0].piece = {
+  move(from:Cell, to:Cell) {
+    return;
+  }
+}
